d93c8d9fc862e832bd9533993863096c6849e6ff
Maihara Santos <maiharagabriellisantos@gmail.com>
2020-11-02 10:04:38 -0800

    Adds changes did a long time ago (I will review it, problem the code is not so good) and adds .exe file

--- SnakeGame.c
+++ SnakeGame.c
@@ -5,9 +5,16 @@
 #include <stdbool.h>
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-int GAME_VEL = 240;
+const int SCREEN_WIDTH = 300;
+const int SCREEN_HEIGHT = 300;
+const int GAME_VEL = 240;
+const int SNAKE_SIZE = 15;
+const int FOOD_SIZE = 10;
+
+//Colors
+SDL_Color WHITE = {0xFF, 0xFF, 0xFF, 0xFF};
+SDL_Color BLUE = {0x00, 0x00, 0xFF, 0xFF};
+SDL_Color GREEN = {0x00, 0xFF, 0x00, 0xFF};
 
 //Enums
 enum Direction
@@ -27,13 +34,13 @@ struct node {
 
 //Game functions
 int Random(int min, int max);
-void DealWithEvents(enum Direction* currDir, bool* exit);
+void DealWithEvents(enum Direction currDir, enum Direction* nextDir, bool* exit);
 SDL_Rect CreateRect(int x, int y, int w, int h);
-void UpdateRect(SDL_Rect* rect, SDL_Renderer* renderer);
+void UpdateRect(SDL_Rect* rect, SDL_Renderer* renderer, SDL_Color edgeColor, SDL_Color fillColor);
 Node* CreateSnake();
-void UpdateSnake(Node** headPtr, Node** tail, enum Direction currDir, bool* exitPtr);
+void UpdateSnake(Node** headPtr, Node** tailPtr, enum Direction* currDir, enum Direction nextDir, bool* bDead);
 void AddNode(Node** head, Node** tail);
-void newGame(SDL_Window** windowPtr);
+void newGame(SDL_Window** windowPtr, bool* exit);
 
 //Required main to SDL
 int main(int argc, char* args[])
@@ -62,8 +69,32 @@ int main(int argc, char* args[])
         }
         else
         {
-            //TODO: Add quit check here
-            newGame(&window);
+            bool exit = false;
+            while(!exit)
+            {
+                newGame(&window, &exit);
+                if (exit)
+                { break; }
+                SDL_Event event;
+                while (1)
+                {
+                    if(SDL_PollEvent(&event))
+                    {
+                        if(event.type == SDL_QUIT)
+                        {
+                            exit = true;
+                            break;
+                        }
+                        else if(event.type == SDL_KEYDOWN)
+                        {
+                            printf("Physical %s key acting as %s key\n",
+                                SDL_GetScancodeName(event.key.keysym.scancode),
+                                SDL_GetKeyName(event.key.keysym.sym));
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -81,7 +112,7 @@ int Random(int min, int max)
     return (int)((max + 1 - min)*((float)rand()/RAND_MAX) + min );
 }
 
-void DealWithEvents(enum Direction* currDir, bool* exit)
+void DealWithEvents(enum Direction currDir, enum Direction* nextDir, bool* exit)
 {
     SDL_Event event;
     if (SDL_PollEvent(&event))
@@ -96,16 +127,16 @@ void DealWithEvents(enum Direction* currDir, bool* exit)
                 switch (keysym.scancode)
                 {
                     case SDL_SCANCODE_DOWN:
-                        *currDir = *currDir == UP ? UP:DOWN;
+                        *nextDir = currDir == UP ? UP:DOWN;
                         break;
                     case SDL_SCANCODE_LEFT:
-                        *currDir = *currDir == RIGHT ? RIGHT : LEFT;
+                        *nextDir = currDir == RIGHT ? RIGHT : LEFT;
                         break;
                     case SDL_SCANCODE_RIGHT:
-                        *currDir = *currDir == LEFT ? LEFT : RIGHT;
+                        *nextDir = currDir == LEFT ? LEFT : RIGHT;
                         break;
                     case SDL_SCANCODE_UP:
-                        *currDir = *currDir == DOWN ? DOWN:UP;
+                        *nextDir = currDir == DOWN ? DOWN:UP;
                         break;
                 }
                 break;
@@ -125,11 +156,11 @@ SDL_Rect CreateRect(int x, int y, int w, int h)
 }
 
 //TODO: Use SDL_Color structure to receive background color and rect color
-void UpdateRect(SDL_Rect* rect, SDL_Renderer* renderer)
+void UpdateRect(SDL_Rect* rect, SDL_Renderer* renderer, SDL_Color edgeColor, SDL_Color fillColor)
 {
-    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_SetRenderDrawColor(renderer, fillColor.r, fillColor.g, fillColor.b, fillColor.a);
     SDL_RenderFillRect(renderer, rect);
-    SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0xFF, 0xFF);
+    SDL_SetRenderDrawColor(renderer, edgeColor.r, edgeColor.g, edgeColor.b, edgeColor.a);
     SDL_RenderDrawRect(renderer, rect);
 }
 
@@ -138,29 +169,35 @@ Node* CreateSnake()
     Node* head = (Node*)malloc(sizeof(Node));
     head->next = NULL;
     head->pos = 0;
-    head->rect = CreateRect(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 15, 15);
+    head->rect = CreateRect(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SNAKE_SIZE, SNAKE_SIZE);
     return head;
 }
 
-void UpdateSnake(Node** headPtr, Node** tailPtr, enum Direction currDir, bool* exitPtr)
+void UpdateSnake(Node** headPtr, Node** tailPtr, enum Direction* currDir, enum Direction nextDir, bool* bDead)
 {
     Node* aux = *headPtr;
     int dx = 0, dy = 0;
 
-    switch(currDir)
+    switch(*currDir)
     {
         case UP:
-            dy = -15;
+            dy = -SNAKE_SIZE;
             break;
         case DOWN:
-            dy = +15;
+            dy = +SNAKE_SIZE;
             break;
         case LEFT:
-            dx = -15;
+            dx = -SNAKE_SIZE;
             break;
         case RIGHT:
-            dx = +15;
+            dx = +SNAKE_SIZE;
             break;
+    }
+    *currDir = nextDir;
+    if ((*headPtr)->rect.y > (SCREEN_HEIGHT - SNAKE_SIZE) || (*headPtr)->rect.y < 0 || (*headPtr)->rect.x > (SCREEN_WIDTH - SNAKE_SIZE) || (*headPtr)->rect.x < 0)
+    {
+        *bDead = true;
+        return;
     }
     if (*headPtr == *tailPtr)
     {
@@ -172,16 +209,17 @@ void UpdateSnake(Node** headPtr, Node** tailPtr, enum Direction currDir, bool* e
     {
         if (SDL_HasIntersection(&((*headPtr)->rect), &(aux->next->rect)))
         {
-            *exitPtr = true;
+            *bDead = true;
+            return;
         }
         aux = aux->next;
     }
-        (*tailPtr)->next = *headPtr;
-        aux->next = NULL;
-        (*tailPtr)->rect.x = (*headPtr)->rect.x + dx;
-        (*tailPtr)->rect.y = (*headPtr)->rect.y + dy;
-        *headPtr = *tailPtr;
-        *tailPtr = aux;
+    (*tailPtr)->next = *headPtr;
+    aux->next = NULL;
+    (*tailPtr)->rect.x = (*headPtr)->rect.x + dx;
+    (*tailPtr)->rect.y = (*headPtr)->rect.y + dy;
+    *headPtr = *tailPtr;
+    *tailPtr = aux;
 }
 
 void AddNode(Node** head, Node** tail)
@@ -197,59 +235,63 @@ void AddNode(Node** head, Node** tail)
     //printf("pos %d\n", aux->pos);
     aux->next = newNode;
     newNode->pos = aux->pos + 1;
-    newNode->rect = CreateRect(aux->rect.x, aux->rect.y, 15, 15);
+    newNode->rect = CreateRect(aux->rect.x, aux->rect.y, SNAKE_SIZE, SNAKE_SIZE);
 }
 
-void newGame(SDL_Window** windowPtr)
+void newGame(SDL_Window** windowPtr, bool* exit)
 {
     SDL_Surface* screenSurface = NULL;
     SDL_Renderer* screenRenderer = NULL;
     screenSurface = SDL_GetWindowSurface(*windowPtr);
     screenRenderer = SDL_CreateRenderer(*windowPtr,-1,0);
-    //Fill enitre surface (NULL) with solid color
+    //Fill entire surface (NULL) with solid color
     SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x0,0x0,0x0));
     
     Node* head = CreateSnake();
     Node* tail = head;
-    SDL_Rect food = CreateRect(Random(0, SCREEN_WIDTH - 5), Random(0, SCREEN_HEIGHT - 5), 5, 5);
+    SDL_Rect food = CreateRect(Random(0, SCREEN_WIDTH - FOOD_SIZE), Random(0, SCREEN_HEIGHT - FOOD_SIZE), FOOD_SIZE, FOOD_SIZE);
     enum Direction currDir = RIGHT;
+    enum Direction nextDir = currDir;
     int frame = 0;
-    bool exit = false;
-
+    bool bDead = false;
     //Game Loop
-    while(!exit)
+    while(1)
     {
-        //Check collision
-        if (SDL_HasIntersection(&(head->rect), &food))
-        {
-            food.x = Random(0, SCREEN_WIDTH - 5);
-            food.y = Random(0, SCREEN_HEIGHT - 5);
-            AddNode(&head, &tail);
-        }
         //Frame count
         frame++;
         if (frame == GAME_VEL)
         {
             frame = 0;
-            UpdateSnake(&head, &tail, currDir, &exit);
+            UpdateSnake(&head, &tail, &currDir, nextDir, &bDead);
+            if(bDead)
+            {
+                break;
+            }
         }
-        if (head->rect.y > (SCREEN_HEIGHT - head->rect.h) || head->rect.y < 0 || head->rect.x > (SCREEN_WIDTH - head->rect.w) || head->rect.x < 0)
+        //Deal with events
+        DealWithEvents(currDir, &nextDir, exit);
+        if(*exit)
         {
-            exit = true;
+            break;
         }
 
-        //Deal with events
-        DealWithEvents(&currDir, &exit);
+        //Check collision
+        if (SDL_HasIntersection(&(head->rect), &food))
+        {
+            food.x = Random(0, SCREEN_WIDTH - FOOD_SIZE);
+            food.y = Random(0, SCREEN_HEIGHT - FOOD_SIZE);
+            AddNode(&head, &tail);
+        }
         
         SDL_SetRenderDrawColor(screenRenderer, 0x00, 0x00, 0x00, 0xFF);
         SDL_RenderClear(screenRenderer);
         Node* aux = head;
         while(aux != NULL)
         {
-            UpdateRect(&aux->rect, screenRenderer);
+            UpdateRect(&aux->rect, screenRenderer, BLUE, WHITE);
             aux = aux->next;
         }
-        UpdateRect(&food, screenRenderer);
+        UpdateRect(&food, screenRenderer, GREEN, GREEN);
         SDL_RenderPresent(screenRenderer);
     }
     SDL_DestroyRenderer(screenRenderer);
